dfc0ed12815b7b8c1ca8a5a5338a9378fb3542b6
Hamdane Khiari <hamdane.khiari@gmail.com>
2023-12-03 18:53:59 +0100

    Suppression formulaires tests API

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import EventList from './composants/Formulaires/EventList';
-import EventForm from './composants/Formulaires/EventForm';
 import Header from './composants/Header/header';
 import Footer from './composants/Footer/footer';
 
@@ -9,13 +7,6 @@ const App = () => {
   return (
     <Router>
       <Header />
-      <ul>
-        <li><Link to="/events">Événements</Link></li>
-      </ul>
-      <Routes>
-        <Route path="/events" element={<EventList />} />
-        <Route path="/events/add" element={<EventForm />} />
-      </Routes>
       <Footer/>
     </Router>
   );
